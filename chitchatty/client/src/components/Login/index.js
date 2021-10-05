364a180ab654b0bc05af30b91fb29df6395f57d5
Caner <kurucaner@gmail.com>
2021-10-05 18:45:42 -0400

    added form

--- chitchatty/client/src/components/Login/index.js
+++ chitchatty/client/src/components/Login/index.js
@@ -41,7 +41,6 @@ const Login = () => {
     const { username, password, phoneNumber, avatarURL } = form;
 
     const URL = "https://localhost:5000/auth";
-    // const URL = 'https://medical-pager.herokuapp.com/auth';
 
     const {
       data: { token, userId, hashedPassword, fullName },
